691fdedd44c4b556f332e538db74300b7bfcde65
MADHUKIRAN <madhukiran918@gmail.com>
2024-04-25 22:22:23 +0530

    second set of programs

---
+++ src/main/java/Basic_PRO/StarPatterns/StarPattern20.java
@@ -0,0 +1,19 @@
+package Basic_PRO.StarPatterns;
+
+import java.util.Scanner;
+
+public class StarPattern20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = n; i > 0; i--) {
+            for (int j = n; j > i; j--) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print((char)(j+64) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
